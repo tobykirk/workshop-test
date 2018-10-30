da12f4a8e67b136bd599b6cb99f105a9396c30ea
Toby Kirk <toby.kirk@maths.ox.ac.uk>
2018-10-30 17:57:29 +0000

    Added the comments and documentation to mysoftware

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ from ubuntu:18.04
 
 RUN apt-get update && apt-get install -y python3 python3-pip
 
-RUN pip3 install pytest numpy
+RUN pip3 install pytest numpy sphinx
 
 WORKDIR /app
--- mysoftware/functions.py
+++ mysoftware/functions.py
@@ -2,6 +2,30 @@ import numpy as np
 
 
 def square(x):
+    """
+    Takes a number x and squares it
+    
+    Parameters:
+    -----------
+    x, float or int:
+        Number which is to be squared
+
+    Returns:
+    --------
+    float:
+        Square of x
+
+    Examples:
+    ---------
+    >>> square(2)
+    4
+
+    >>> square(8)
+    64
+
+    >>> square(-1)
+    1      
+    """
     return x**2
 
 def coulomb(r):
